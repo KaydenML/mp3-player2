a71c394da824cbfb3f9b9bf43770e4c0d2571907
KaydenML <104690535+KaydenML@users.noreply.github.com>
2022-05-31 22:10:53 -0700

    Update mp3player.py

--- mp3player.py
+++ mp3player.py
@@ -1,9 +1,5 @@
 import vlc
-import os
-cwd = os.getcwd()
-p = vlc.MediaPlayer(r"C:\Users\kayde\Downloads\natoriousthugs.mp3")
+p = vlc.MediaPlayer(r"C:\Users\kayde\Documents\GitHub\mp3-player2\natoriousthugs.mp")
 p.play()
-print("Path at terminal when executing this file")
-print(os.getcwd() + "\n")
 while True:
     pass
